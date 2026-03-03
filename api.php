d7f744fc6e1f28b76d7daf2e98d2975375d05f5e
0ppangz <fairydevz@gmail.com>
2026-03-03 19:51:05 +0700

    bug fixing

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 // For handling the API, see the docs in here.
 // https://dummyjson.com/docs/
-$api = 'https://dummyjson.com/recipes?limit=10&skip=200';
+$api = 'https://dummyjson.com/recipes';
 $data = json_decode(file_get_contents($api),true);
 
